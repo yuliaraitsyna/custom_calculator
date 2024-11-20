1c18d26e344899dfee4deb54d62c8d80b48ef288
yuliaraitsyna <yuliaraitsyna@gmail.com>
2024-11-20 15:36:59 +0300

    feat: implement theme switcher

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './styles.css';
+import { Theme } from './model/theme';
+
+const themeButton = document.getElementById('theme-btn');
+themeButton.textContent = `Switch to ${Theme.DARK} mode`;
+
+themeButton.addEventListener('click', () => {
+  const htmlElement = document.documentElement;
+
+  if (htmlElement.getAttribute('data-theme') === Theme.LIGHT) {
+    htmlElement.setAttribute('data-theme', Theme.DARK);
+    themeButton.textContent = `Switch to ${Theme.LIGHT} mode`;
+  } else {
+    htmlElement.setAttribute('data-theme', Theme.LIGHT);
+    themeButton.textContent = `Switch to ${Theme.DARK} mode`;
+  }
+});
